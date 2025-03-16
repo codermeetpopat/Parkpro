8e2b7c4dddf1445d597336d88cb198475b29a000
codermeetpopat <144238847+codermeetpopat@users.noreply.github.com>
2025-03-16 20:02:35 +0530

    file upload

---
+++ vendors/scripts/advanced-components.js
@@ -0,0 +1,37 @@
+// Switchery
+		var elems = Array.prototype.slice.call(document.querySelectorAll('.switch-btn'));
+		$('.switch-btn').each(function() {
+			new Switchery($(this)[0], $(this).data());
+		});
+
+		// Bootstrap Touchspin
+		$("input[name='demo_vertical2']").TouchSpin({
+			verticalbuttons: true,
+			// verticalupclass: 'fa fa-plus',
+			// verticaldownclass: 'fa fa-minus'
+		});
+		$("input[name='demo3']").TouchSpin();
+		$("input[name='hour']").TouchSpin({
+			min: 0,
+			max: 100,
+			step: 1,
+			decimals: 0,
+			boostat: 5,
+			maxboostedstep: 10,
+			postfix: "Hour"
+			
+		});
+		$("input[name='price']").TouchSpin({
+			min: -1000000000,
+			max: 1000000000,
+			stepinterval: 50,
+			maxboostedstep: 10000000,
+			prefix: "₹"
+		});
+		$("input[name='demo3_22']").TouchSpin({
+			initval: 40
+		});
+		$("input[name='demo5']").TouchSpin({
+			prefix: "pre",
+			postfix: "post"
+		});
